1237292a25bfdcdddbbcefeee6d7a271f868fa1d
GoenkoAlina <a.goenko@g.nsu.ru>
2024-05-31 06:37:44 +0700

    server-client

--- server-client/main.py
+++ server-client/main.py
@@ -1,31 +1,14 @@
-from server import Server
-from client import Client
-import threading
 import unittest
-import time
 
 
 class UnitTests(unittest.TestCase):
     def test_name(self):
-        self.assertEqual('Alina', 'Alina')
+        self.assertEqual(1, 1)
 
 
 def main():
     unittest.main()
-
-# def main():
-#     server = Server()
-#     server.bind()
-#     server.server_start()
-
-# def main():
-# 	client = Client()
-# 	client.bind()
-# 	receive = threading.Thread(target=client.receive)
-# 	receive.start()
-# 	client.client_start()
-# 	receive.join()
-
+    
 
 if __name__ == "__main__":
     main()
