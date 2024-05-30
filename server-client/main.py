16d0ea0c36ec21a53c614e7be6070ee997f341db
GoenkoAlina <a.goenko@g.nsu.ru>
2024-05-31 06:41:48 +0700

    server-client

--- server-client/main.py
+++ server-client/main.py
@@ -4,6 +4,9 @@ import unittest
 class UnitTests(unittest.TestCase):
     def test_name(self):
         self.assertEqual(1, 1)
+    
+    def test_one(self):
+        self.assertFalse(1 == 2)
 
 
 def main():
